fbd3cdda4741ec3164a0aab89705bf00e99ae078
Ivan <ziswp1@gmail.com>
2023-10-05 10:58:03 +0300

    init go

--- app/src/main/java/com/example/retrofit/ui/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/ui/MainActivity.kt
@@ -40,8 +40,9 @@ class MainActivity : AppCompatActivity() {
        // val repo = Repository(JsonPlaceHolderSingleton.api)
        // val vmFactory = MainViewModelFactory(repo)
        // viewModel = ViewModelProviders.of(this, vmFactory).get(MainViewModel::class.java)
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         clickButton()
 
 
@@ -135,5 +136,65 @@ class MainActivity : AppCompatActivity() {
             adapter = PhotoDataAdapter(photoDataList)
         }
     }
+
+    /*learn rx
+    private fun showDataRx() {
+        var arrList: ArrayList<Client> = ArrayList()
+        arrList.add(Client("Vasy", 22, true))
+        arrList.add(Client("Gena", 42, true))
+        arrList.add(Client("Vany", 65, false))
+        arrList.add(Client("Marry", 82, true))
+        arrList.add(Client("Fedor", 21, true))
+        arrList.add(Client("Mike", 26, false))
+        arrList.add(Client("Tod", 63, true))
+        arrList.add(Client("Heriman", 29, false))
+        arrList.add(Client("Gher", 98, true))
+        arrList.add(Client("oleg", 36, false))
+
+        /* arrList.stream()
+             .filter(Client::active)
+             .map(Client::name)
+             .forEach(System.out::println)
+ */
+
+        // System.out::println("End stream")
+
+
+
+        val flowClient: Flowable<Client> = Flowable.create(FlowableOnSubscribe { emitter -> // signal an item
+            /* emitter.onNext("Hello")
+
+             // could be some blocking operation
+             Thread.sleep(1000)
+
+             // the consumer might have cancelled the flow
+             if (emitter.isCancelled) {
+                 return@FlowableOnSubscribe
+             }
+             emitter.onNext("World")
+             Thread.sleep(1000)
+ */
+            // the end-of-sequence has to be signaled, otherwise the
+            // consumers may never finish
+            for (c in arrList) {
+                emitter.onNext(c)
+            }
+            emitter.onComplete()
+        }, BackpressureStrategy.BUFFER)
+
+
+        flowClient
+            .observeOn(io.reactivex.rxjava3.schedulers.Schedulers.computation())
+            .subscribeOn(io.reactivex.rxjava3.schedulers.Schedulers.io())
+            .filter(Client::active)
+            .map(Client::name)
+            .observeOn(io.reactivex.rxjava3.schedulers.Schedulers.io())
+            .forEach(System.out::println)
+
+    }
+    data class Client(val name: String, val sum: Int, val active: Boolean)
+
+    learn rx*/
+
 
 }
